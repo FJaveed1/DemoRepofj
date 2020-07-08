69edb559fda0f71ae322678ff0143980ba556b9d
Fouzia Javeed <fouzia01@gmail.com>
2020-07-08 18:31:48 -0400

    First Git commit for my demo project

---
+++ src/main/java/com/qa/libs/TestBase.java
@@ -0,0 +1,10 @@
+package com.qa.libs;
+
+public class TestBase {
+	
+	public void Test() {
+		System.out.println("This is Test Method!!");
+	}
+
+
+}
---
+++ src/main/java/com/qa/pages/HomPage.java
@@ -0,0 +1,8 @@
+package com.qa.pages;
+
+public class HomPage {
+	public void HomPage() {
+		System.out.println("This is Home Page!!");
+	}
+
+}
