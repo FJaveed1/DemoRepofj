5064c694e50afa31971774f6c2dee588e502c950
FJaveed1 <60159958+FJaveed1@users.noreply.github.com>
2020-07-10 09:22:01 -0400

    Update TestUtil.java
    Added the constructor

--- src/main/java/com/qa/util/TestUtil.java
+++ src/main/java/com/qa/util/TestUtil.java
@@ -2,6 +2,11 @@ package com.qa.util;
 
 public class TestUtil {
 	
+	public TestUtil()
+	{
+		System.out.println("This is a constuctor added!");
+	}
+	
 	public void testUtil() {
 		System.out.println("This is TestUtil Page!!");
 	}
